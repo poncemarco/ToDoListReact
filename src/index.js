584dd259c4455412d3bda5b124bae452c00aac41
Marco Ponce <95949775+poncemarco@users.noreply.github.com>
2023-05-30 11:18:21 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//CHALLENGE: Make this app work by applying what you've learnt.
+//1. When new text is written into the input, its state should be saved.
+//2. When the add button is pressed, the current data in the input should be
+//added to an array.
+//3. The <ul> should display all the array items as <li>s
+
+/*const citrus = ["Lime", "Lemon", "Orange"]
+const fruits = ["Apple", "Banana", "Coconut", ...citrus]
+console.log(fruits)*/
+// This is how you can concat arrays
